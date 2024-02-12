a6b873aec09e66807856d3764d87141bd2394d40
Samuel Amphoux <amphoux.s@hotmail.fr>
2024-02-12 11:13:47 +0100

    refactor: move logic to appropriate MVC parts

---
+++ src/js/model.js
@@ -0,0 +1,40 @@
+// import {async} from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (recipeId) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`🚫 ${data.message} Error:${res.status} 🚫`);
+
+    const {
+      id,
+      title,
+      publisher,
+      source_url: sourceUrl,
+      image_url: image,
+      servings,
+      cooking_time: cookingTime,
+      ingredients,
+    } = data.data.recipe;
+
+    state.recipe = {
+      id,
+      title,
+      publisher,
+      sourceUrl,
+      image,
+      servings,
+      cookingTime,
+      ingredients,
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
