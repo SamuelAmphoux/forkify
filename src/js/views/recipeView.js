830df761b43850e538fa37f87ea5586a38c78c66
Samuel Amphoux <amphoux.s@hotmail.fr>
2024-02-29 17:20:36 +0100

    fix: correct synthax according to fracty implementation

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -134,7 +134,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ingredient.quantity ? new fracty(ingredient.quantity).toString() : ''
+        ingredient.quantity ? fracty(ingredient.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ingredient.unit}</span>
